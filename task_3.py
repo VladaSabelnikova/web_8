692aa3a65393b7766c9ac12c93dc006d56a62308
Vlada Sabelnikova <vladasabelnikova@gmail.com>
2022-02-09 20:10:13 +0300

    Третья задача решена
    Всё.

---
+++ task_3.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+from flask import Flask, render_template
+
+from src.const import ALL_PROFESSIONS
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title: str) -> render_template:
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof: str) -> render_template:
+    return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst: str) -> Union[render_template, str]:
+
+    if lst not in ('ol', 'ul'):
+        return f'Неверный параметр {lst}'
+
+    return render_template(
+        'list_prof.html',
+        lst=lst,
+        professions=ALL_PROFESSIONS
+    )
+
+
+def main():
+    app.run(port=8080, host='')
+
+
+if __name__ == '__main__':
+    main()
